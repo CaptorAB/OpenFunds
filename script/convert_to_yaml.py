c017062e4cc31935003696e370a58b2bb9548ec7
Andreas Lindh <andreas.lindh@loopstone.com>
2024-01-04 05:19:59 +0100

    started on fieldlist.yaml

--- script/convert_to_yaml.py
+++ script/convert_to_yaml.py
@@ -1,13 +1,13 @@
 #/bin/env python3
-import sys
+
 import argparse
-import datetime
 from pprint import pprint
 import yaml
-import re
 import unicodedata
+import re
 
-
+ID_PATTERN = re.compile(r'OF-ID\W+\*\*(.*)\*\*\*\*\*\*', re.DOTALL)
+ID_PATTERN = re.compile(r'OF-ID\s+\*\*(\w+?)\*\*\s+Field Name\s+\*\*(\w+)\n(\w+)\*\*\*\*\*\*', re.DOTALL)
 
 
 
@@ -21,6 +21,18 @@ if __name__ == "__main__":
 
     chunks = text.split("\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_")
 
+    fields = []
+
     for chunk in chunks:
         text = unicodedata.normalize('NFKD', chunk)
-        pprint(text)
+
+        m = re.search(ID_PATTERN, text)
+        if re.search(ID_PATTERN, text):
+            print(m)
+            print(m.groups())
+            fields.append({"OF-ID": m.groups()[0], "Field Name": m.groups()[1] + " " + m.groups()[2]})
+
+        #pprint(text)
+
+    with open("fieldlist.yaml", "w") as file:
+        yaml.dump(fields, file)
